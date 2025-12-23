9f2476b2ce91531f683e569032a52e55dcefe799
Sonnyzera <meninolucashotwheels@gmail.com>
2025-12-23 15:44:33 -0300

    botões de salvar e baixar no github

---
+++ baixar_do_github.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ==========================================
+echo    BAIXANDO ALTERACOES DO GITHUB
+echo ==========================================
+echo.
+echo Buscando novidades na nuvem...
+git pull
+echo.
+echo ==========================================
+echo          SUCESSO! TUDO ATUALIZADO.
+echo ==========================================
+pause

277b932dc11d43793039906f8f07866cfbbaa066
Sonnyzera <meninolucashotwheels@gmail.com>
2026-01-22 12:27:29 -0300

    Restore production configuration

---
+++ baixar_main.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ==========================================
+echo    BAIXANDO ALTERACOES DO MAIN
+echo ==========================================
+echo.
+echo Buscando novidades na nuvem...
+git pull origin main
+echo.
+echo ==========================================
+echo          SUCESSO! TUDO ATUALIZADO DO MAIN.
+echo ==========================================
+pause
